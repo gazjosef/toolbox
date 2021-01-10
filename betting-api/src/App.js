99a96f5dfde84e4a393e263c69040ce01b84b004
Gareth Hind <garethjhind@gmail.com>
2021-01-10 22:45:34 +1100

    Updated betting-api

--- betting-api/src/App.js
+++ betting-api/src/App.js
@@ -1,22 +1,15 @@
-// import logo from './logo.svg';
 import './App.scss';
+
+import { SearchBar } from './components/SearchBar/SearchBar'
+import { Output } from './components/Output/Output'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>$$$ Odds Checker</h1>
+        <SearchBar />
+        <Output />
       </header>
     </div>
   );
---
+++ betting-api/src/components/Output/Output.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Output = () => {
+    return (
+        <div>
+            <h1>Output</h1>
+        </div>
+    )
+}
