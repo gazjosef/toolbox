185d410ebf5c14493e732ea7d56928dcf1af3341
Gareth Hind <garethjhind@gmail.com>
2021-01-13 21:37:07 +1100

    Updated mockup

--- betting-api/src/App.js
+++ betting-api/src/App.js
@@ -4,6 +4,13 @@ import { SearchBar } from './components/SearchBar/SearchBar'
 import { Output } from './components/Output/Output'
 
 function App() {
+  // Output
+  // let outputEl = document.getElementById('output');
+  // let tableEl = document.getElementById('table');
+  // let sportEl = document.getElementById('selectSport');
+  // let compEl = document.getElementById('selectComp');
+  // let marketEl = document.getElementById('selectMarket');
+// let regionEl = document.getElementById('selectRegion');
   return (
     <div className="App">
       <header className="App-header">
