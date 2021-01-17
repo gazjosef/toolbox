84d6965add5b65730388460b0154b2850a82568e
Gareth Hind <garethjhind@gmail.com>
2021-01-17 22:23:18 +1100

    Updated complete pool care

--- complete-pool-care2/src/App.js
+++ complete-pool-care2/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.scss';
+
+// Pages
+import { Home } from './components/pages/home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      COMPLETE POOL CARE
+      <Home />
     </div>
   );
 }
---
+++ complete-pool-care2/src/components/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Layout
+import { Header } from "../../layout/header/Header"
+
+export const Home = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
