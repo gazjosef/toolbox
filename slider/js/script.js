d3bd87c9f53ad750148165c924bf1e091d83beab
Gareth Hind <garethjhind@gmail.com>
2021-10-27 12:04:17 +1100

    Updated Mock-Up - Slide Show

--- slider/js/script.js
+++ slider/js/script.js
@@ -1,18 +1,30 @@
-let slideIndex = 0;
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function showSlides(n) {
   let slides = document.getElementsByClassName("slideshow__slides");
   let dots = document.getElementsByClassName("dots__dot");
   let i;
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  // for (i = 0; i < slides.length; i++) {
+  //   slides[i].style.display = "none";
+  // }
+
+  // slideIndex++;
+
+  if (n > slides.length) {
+    slideIndex = 1;
   }
 
-  slideIndex++;
-
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
 
   for (i = 0; i < dots.length; i++) {
@@ -22,7 +34,10 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
+  for (i = 0; i < slides.length; i++) {
+    console.log("for loop", i);
+    // setTimeout(showSlides(i + 1), 3000); // Change image every 3 seconds
+  }
 }
 
 showSlides();
