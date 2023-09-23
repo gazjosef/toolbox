58a846df139aa26b6a43b54a0e261c4895349ed0
Gareth Hind <garethjhind@gmail.com>
2023-09-23 16:56:02 +1000

    Updated next-ts-testing

--- frontend/boilerplate/next-ts-testing/__tests__/Home.test.tsx
+++ frontend/boilerplate/next-ts-testing/__tests__/Home.test.tsx
@@ -1,10 +1,30 @@
 import { render, screen } from "@testing-library/react";
 import Home from "@/app/page";
 
-it("should have Docs text", () => {
-  render(<Home />); //ARANGE
+describe("Home", () => {
+  it("should have Docs text", () => {
+    render(<Home />); //ARANGE
 
-  const myElem = screen.getByText("Docs"); // ACT
+    const myElem = screen.getByText("Docs"); // ACT
 
-  expect(myElem).toBeInTheDocument(); // ASSERT
+    expect(myElem).toBeInTheDocument(); // ASSERT
+  });
+
+  it("should contain the text 'information'", () => {
+    render(<Home />); //ARANGE
+
+    const myElem = screen.getByText(/information/i); // ACT
+
+    expect(myElem).toBeInTheDocument(); // ASSERT
+  });
+
+  it("should have a heading", () => {
+    render(<Home />); //ARANGE
+
+    const myElem = screen.getByRole("heading", {
+      name: "Learn",
+    }); // ACT
+
+    expect(myElem).toBeInTheDocument(); // ASSERT
+  });
 });
