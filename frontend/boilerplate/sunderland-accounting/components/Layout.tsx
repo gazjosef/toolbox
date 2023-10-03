f17f2ee157b62d2a52af8af9c45154b3d365544a
Gareth Hind <garethjhind@gmail.com>
2023-10-03 21:04:32 +1100

    Updated Sunderland-Accounting

---
+++ frontend/boilerplate/sunderland-accounting/components/Layout.tsx
@@ -0,0 +1,31 @@
+import Meta from "./Meta.tsx";
+import { useRouter } from "next/router";
+
+// import Topbar from "@/components/Layout/Topbar";
+// import Header from "@/components/Layout/Header"; // Commented out as per your code
+
+// import Footer from "@/components/Layout/Footer";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const router = useRouter();
+
+  return (
+    <>
+      <header>
+        <Meta />
+      </header>
+      {/* <Topbar /> */}
+      {/* <Header /> */}
+      {router.pathname === "/"}{" "}
+      {/* It seems like there's a condition but not being used */}
+      <main>{children}</main>
+      {/* <Footer /> */}
+    </>
+  );
+};
+
+export default Layout;
---
+++ frontend/boilerplate/sunderland-accounting/components/Meta.tsx
@@ -0,0 +1,50 @@
+import Head from "next/head";
+
+interface MetaProps {
+  title?: string;
+  keywords?: string;
+  description?: string;
+}
+
+const Meta: React.FC<MetaProps> = ({
+  title = "Sunderland Accounting",
+  keywords = "Accounts Payable, Accounts Receivable, ...", // Your long keywords list here
+  description = "Our accounting firm provides expert bookkeeping, ...", // Your default description here
+}) => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <meta charSet="utf-8" />
+
+      {/* Favicon: Accounting Icon */}
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+
+      <title>{title}</title>
+    </Head>
+  );
+};
+
+export default Meta;
