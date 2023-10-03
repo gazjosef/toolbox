3969f58d5fad891f7d5e009510fec210bac89868
Gareth Hind <garethjhind@gmail.com>
2023-10-03 22:20:10 +1100

    Updated Sunderland-Accounting

--- frontend/boilerplate/sunderland-accounting/components/Section/Hero.tsx
+++ frontend/boilerplate/sunderland-accounting/components/Section/Hero.tsx
@@ -1,4 +1,25 @@
+import Link from "next/link";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <article id="home" className="article-home u-relative">
+      {/* <Header /> */}
+      <div className="container">
+        <h1 className="heading-one">
+          SMARTER WAY TO FILE TAX RETURN{" "}
+          <span>IN PERSON. BY PHONE. ONLINE</span>
+        </h1>
+        <p className="paragraph">
+          We help lodging tax return Sydney from anywhere. Get your biggest tax
+          refund with no tax knowledge needed.
+        </p>
+        <button className="btn btn--home">
+          {/* <Link href="#">
+            <a>Book Appointment</a>
+          </Link> */}
+        </button>
+      </div>
+    </article>
+  );
 };
 export default Hero;
