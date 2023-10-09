2683a0cdce85b3e52a8f1feb89be647088a43644
Gareth Hind <garethjhind@gmail.com>
2023-10-09 21:08:33 +1100

    Updated Sunderland-Accounting

--- frontend/boilerplate/sunderland-accounting/pages/contact.tsx
+++ frontend/boilerplate/sunderland-accounting/pages/contact.tsx
@@ -1,3 +1,7 @@
+import { FaPhone } from "react-icons/fa";
+import MapContainer from "../components/Snippets/Map/Map.tsx";
+import useGsapFadeIn from "../hooks/useGsapFadeIn";
+
 const Contact = () => {
   return <div>Contact</div>;
 };
