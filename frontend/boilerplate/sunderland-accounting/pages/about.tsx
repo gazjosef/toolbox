3f075436455bef92cb69af475c7c0354605c43e8
Gareth Hind <garethjhind@gmail.com>
2023-10-10 21:47:51 +1100

    Updated Sunderland-Accounting

--- frontend/boilerplate/sunderland-accounting/pages/about.tsx
+++ frontend/boilerplate/sunderland-accounting/pages/about.tsx
@@ -8,33 +8,84 @@ interface TeamMember {
   image_src: string;
 }
 
-const team: TeamMember[] = [
-  {
-    name: "Jamie Ryder",
-    position: "Founding Partner & Chairman",
-    image_src:
-      "/images/team/tinified/linkedin-sales-solutions-pAtA8xe_iVM-unsplash.jpg",
-  },
-  {
-    name: "Jenete Ekwueme",
-    position: "Managing Partner",
-    image_src:
-      "/images/team/tinified/christina-wocintechchat-com-SJvDxw0azqw-unsplash.jpg",
-  },
-  {
-    name: "Isabella Telfer",
-    position: "Associate Director",
-    image_src:
-      "/images/team/tinified/mateus-campos-felipe-WnPJft0DJpk-unsplash.jpg",
-  },
-  {
-    name: "Jing Chao",
-    position: "Accounting & Tax Manager",
-    image_src:
-      "/images/team/tinified/christina-wocintechchat-com-0Zx1bDv5BNY-unsplash.jpg",
-  },
-];
 const About: React.FC = () => {
-  return <div>About</div>;
+  const [aboutTitleRef, aboutTextRef, aboutGalleryRef] = useGsapFadeIn();
+
+  const team: TeamMember[] = [
+    {
+      name: "Jamie Ryder",
+      position: "Founding Partner & Chairman",
+      image_src:
+        "/images/team/tinified/linkedin-sales-solutions-pAtA8xe_iVM-unsplash.jpg",
+    },
+    {
+      name: "Jenete Ekwueme",
+      position: "Managing Partner",
+      image_src:
+        "/images/team/tinified/christina-wocintechchat-com-SJvDxw0azqw-unsplash.jpg",
+    },
+    {
+      name: "Isabella Telfer",
+      position: "Associate Director",
+      image_src:
+        "/images/team/tinified/mateus-campos-felipe-WnPJft0DJpk-unsplash.jpg",
+    },
+    {
+      name: "Jing Chao",
+      position: "Accounting & Tax Manager",
+      image_src:
+        "/images/team/tinified/christina-wocintechchat-com-0Zx1bDv5BNY-unsplash.jpg",
+    },
+  ];
+  return (
+    <article id="about" className="article-about">
+      <div className="article-about__about container">
+        <section className="article-about__title" ref={aboutTitleRef}>
+          <h2 className="heading-two">About Us</h2>
+        </section>
+        <section className="article-about__text" ref={aboutTextRef}>
+          <p className="paragraph">
+            We are a professional accounting firm dedicated to providing
+            top-notch financial services to businesses and individuals. Our team
+            of experts has a combined experience of several decades in the field
+            of accounting and finance. We offer a wide range of services
+            including bookkeeping, tax planning and preparation, financial
+            statement analysis, and consulting.
+          </p>
+          <p className="paragraph">
+            Our goal is to help our clients make informed financial decisions,
+            stay compliant with tax laws, and achieve their financial goals. We
+            are committed to providing personalized, timely and efficient
+            services at an affordable price.
+          </p>
+        </section>
+      </div>
+
+      <div className="container">
+        <section className="article-about__gallery" ref={aboutGalleryRef}>
+          {team.map((member, index) => {
+            return (
+              <section className="article-about__team | u-relative" key={index}>
+                <div className="section-about__img">
+                  <Image
+                    src={member.image_src}
+                    alt={member.name}
+                    objectFit="contain"
+                    layout="fill"
+                  />
+                </div>
+                <div className="article-about__desc">
+                  <h3 className="heading-three">{member.position}</h3>
+                  <h3 className="heading-three heading-three--lighter">
+                    {member.name}
+                  </h3>
+                </div>
+              </section>
+            );
+          })}
+        </section>
+      </div>
+    </article>
+  );
 };
 export default About;
--- frontend/boilerplate/sunderland-accounting/pages/contact.tsx
+++ frontend/boilerplate/sunderland-accounting/pages/contact.tsx
@@ -1,10 +1,88 @@
+import React from "react";
 import { FaPhone } from "react-icons/fa";
-import MapContainer from "../components/Snippets/Map/Map.tsx";
+// import MapContainer from "@/components/Map/MapContainer";
 import useGsapFadeIn from "../hooks/useGsapFadeIn";
 
 const Contact = () => {
   const [contactTitleRef, contactMapRef, contactDetailsRef] = useGsapFadeIn();
 
-  return <div>Contact</div>;
+  return (
+    <section id="contact" className="article-contact">
+      <div className="container">
+        <section className="article-contact__title" ref={contactTitleRef}>
+          <h2 className="heading-two u-margin-bottom-small">Contact Us</h2>
+        </section>
+
+        <section className="article-contact__contact" ref={contactMapRef}>
+          <section className="article-contact__map">
+            {/* <MapContainer /> */}
+          </section>
+
+          <section className="article-contact__details">
+            <h3 className="heading-three">Address</h3>
+            <p className="paragraph">
+              5/8 Ave of the Americas <br />
+              Newington NSW 2127
+            </p>
+            <h3 className="heading-three">Hours</h3>
+            <p className="paragraph">
+              Sun: Closed <br />
+              Mon: 8:30am - 5:30pm <br />
+              Tue: 8:30am - 5:30pm <br />
+              Wed: 8:30am - 5:30pm <br />
+              Thu: 8:30am - 5:30pm <br />
+              Fri: 8:30am - 5:30pm <br />
+              Sat: Closed <br />
+            </p>
+            <h3 className="heading-three">Email</h3>
+            <p className="paragraph">
+              Email: info@sunderland.com.au <br />
+            </p>
+            <button className="btn btn--contact | u-flex u-items-center u-justify-between">
+              <FaPhone className="u-mx-0-5" /> 02 9644 9220
+            </button>
+          </section>
+
+          <section className="article-contact__form">
+            <form action="/action_page.php" className="form">
+              <label htmlFor="fname">First Name</label>
+              <input
+                type="text"
+                id="fname"
+                name="firstname"
+                placeholder="Your name.."
+              />
+
+              <label htmlFor="lname">Last Name</label>
+              <input
+                type="text"
+                id="lname"
+                name="lastname"
+                placeholder="Your last name.."
+              />
+
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Your Email"
+              />
+
+              <label htmlFor="subject">Subject</label>
+              <textarea
+                id="subject"
+                name="subject"
+                placeholder="Write something.."
+                style={{ height: "130px" }}
+              ></textarea>
+
+              <input type="submit" value="Submit" className="btn" />
+            </form>
+          </section>
+        </section>
+      </div>
+    </section>
+  );
 };
 export default Contact;
