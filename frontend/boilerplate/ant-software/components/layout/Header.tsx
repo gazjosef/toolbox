8c15216966c0d76c5d5190d5be15ec3531b3543a
Gareth Hind <garethjhind@gmail.com>
2023-10-16 21:17:07 +1100

    Updated

--- frontend/boilerplate/ant-software/components/layout/Header.tsx
+++ frontend/boilerplate/ant-software/components/layout/Header.tsx
@@ -1,6 +1,25 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Navbar from "./Navbar";
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header | u-bg-white-0">
+      <div className="container | u-flex u-items-center u-justify-between">
+        <Link href="#home">
+          <Image
+            src="/img/black-logo.png"
+            alt="Ant Software"
+            className="header__logo"
+            width={196.88}
+            height={55.81}
+          />
+        </Link>
+
+        <Navbar />
+      </div>
+    </header>
+  );
 };
 export default Header;
