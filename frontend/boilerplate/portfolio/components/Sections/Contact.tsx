422b65f8be248d980f0b131414a06d8ab66561c2
Gareth Hind <garethjhind@gmail.com>
2023-10-21 23:24:50 +1100

    Updated

--- frontend/boilerplate/portfolio/components/Sections/Contact.tsx
+++ frontend/boilerplate/portfolio/components/Sections/Contact.tsx
@@ -1,6 +1,71 @@
-import React from "react";
+import React, { useRef } from "react";
+import Image from "next/image";
+import { FaPhone, FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
+import useGsapFadeDown from "../../hooks/useGsapFadeDown";
 
 const Contact: React.FC = () => {
-  return <div>Contact</div>;
+  const [contactTitleRef, contactImgRef, contactDetailsRef] = useGsapFadeDown();
+
+  return (
+    <section id="contact" className="section">
+      <div className="container | u-mx-auto u-grid u-even-columns">
+        <section
+          className="article-contact__image"
+          ref={contactImgRef as React.RefObject<HTMLElement>}
+        >
+          <Image
+            src="/images/section/undraw_designer_re_5v95.svg"
+            alt="responsive"
+            width={960}
+            height={572}
+          />
+        </section>
+
+        <section>
+          <section
+            className="article-contact__details | u-bg-white-0 u-mx-auto"
+            ref={contactDetailsRef as React.RefObject<HTMLElement>}
+          >
+            <h1 className="heading-one | u-margin-bottom-small">Contact</h1>
+            <div>
+              <h3 className="heading-three | u-flex u-items-center">
+                <FaPhone className="u-mr-1" /> Mobile
+              </h3>
+              <p className="paragraph">0402 830 315</p>
+            </div>
+
+            <div>
+              <h3 className="heading-three | u-flex u-items-center">
+                <FaEnvelope className="u-mr-1" /> Email
+              </h3>
+              <p className="paragraph">garethjhind@gmail.com</p>
+            </div>
+
+            <div>
+              <h3 className="heading-three | u-flex u-items-center">
+                <FaGithub className="u-mr-1" /> Github
+              </h3>
+              <p className="paragraph">
+                <a href="https://github.com/gazjosef">
+                  https://github.com/gazjosef
+                </a>
+              </p>
+            </div>
+
+            <div>
+              <h3 className="heading-three | u-flex u-items-center">
+                <FaLinkedin className="u-mr-1" /> LinkedIn
+              </h3>
+              <p className="paragraph">
+                <a href="https://www.linkedin.com/in/gareth-h-a32448191/">
+                  https://www.linkedin.com/in/gareth-h-a32448191/
+                </a>
+              </p>
+            </div>
+          </section>
+        </section>
+      </div>
+    </section>
+  );
 };
 export default Contact;
--- frontend/boilerplate/portfolio/components/Sections/Story.tsx
+++ frontend/boilerplate/portfolio/components/Sections/Story.tsx
@@ -1,6 +1,62 @@
 import React from "react";
+import Image from "next/image";
+import { FaArrowCircleRight } from "react-icons/fa";
+import useGsapFadeDown from "../../hooks/useGsapFadeDown";
 
 const Story: React.FC = () => {
-  return <div>Story</div>;
+  const [storyTitleRef, storyImgRef, storyTextRef] = useGsapFadeDown();
+
+  return (
+    <section id="story" className="section">
+      <div className="container | u-mx-auto">
+        <section className="article-story__title">
+          <h1
+            className="heading-one | u-mb-lg u-center-text"
+            ref={storyTitleRef}
+          >
+            Story
+          </h1>
+        </section>
+      </div>
+      <div className="container | u-mx-auto u-grid u-even-columns u-gap-5">
+        <section ref={storyImgRef as React.RefObject<HTMLElement>}>
+          <Image
+            src="/images/section/undraw_programming_re_kg9v.svg"
+            alt="responsive"
+            width={1000}
+            height={700}
+          />
+        </section>
+        <section
+          className="article-story__text"
+          ref={storyTextRef as React.RefObject<HTMLElement>}
+        >
+          <p className="paragraph">
+            With expertise in HTML, CSS, and SASS, I craft beautiful and
+            responsive user interfaces.
+          </p>
+          <p className="paragraph">
+            I excel in React, Shopify and headless CMS implementations,
+            optimizing e-commerce experiences and leveraging platforms like
+            Sanity.io for seamless content management.
+          </p>
+          <p className="paragraph">
+            Let&lsquo;s collaborate to turn your vision into a successful
+            reality!
+          </p>
+
+          <button className="btn btn--cta btn--inverse | u-max-w-max u-py-2">
+            <a
+              href="#portfolio"
+              className="u-flex u-items-center u-justify-center"
+            >
+              Portfolio
+              <FaArrowCircleRight className="u-ml-1" />
+            </a>
+          </button>
+        </section>
+      </div>
+    </section>
+  );
 };
 export default Story;
