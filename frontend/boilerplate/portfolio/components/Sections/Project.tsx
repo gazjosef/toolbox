8bbee8ef5db078969c8bd23b6717d9b9426f3dc0
Gareth Hind <garethjhind@gmail.com>
2023-11-06 21:38:36 +1100

    Updated

--- frontend/boilerplate/portfolio/components/Sections/Project.tsx
+++ frontend/boilerplate/portfolio/components/Sections/Project.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { FaEye, FaGithub } from "react-icons/fa";
 import { IconContext } from "react-icons";
+import Image from "next/image";
 
 interface ProjectProps {
   project: {
     title: string;
+    cover_image: string;
     excerpt: string;
     categories: string[];
     tag: string;
@@ -16,15 +18,26 @@ interface ProjectProps {
 const Project: React.FC<ProjectProps> = ({ project }) => {
   return (
     <div className="project">
+      <a href={`${project.live}`}>
+        {project.cover_image && (
+          <Image
+            src={project.cover_image}
+            alt={project.title}
+            height={250}
+            width={500}
+            className="project__image"
+          />
+        )}
+      </a>
       <h1>{project.title}</h1>
       <p>{project.excerpt}</p>
       <div>
-        <a className="post__button" href={`${project.github}`}>
+        <a className="project__button" href={`${project.github}`}>
           <IconContext.Provider value={{}}>
             <FaGithub /> READ
           </IconContext.Provider>
         </a>
-        <a className="post__button" href={`${project.live}`}>
+        <a className="project__button" href={`${project.live}`}>
           <IconContext.Provider value={{}}>
             <FaEye /> LIVE
           </IconContext.Provider>
