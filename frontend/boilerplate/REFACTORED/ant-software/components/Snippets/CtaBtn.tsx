a078c75880aed104d2b305777e4727c4b68ce505
Gareth Hind <garethjhind@gmail.com>
2024-01-27 21:46:32 +1100

    Styles: CTA Btn

--- frontend/boilerplate/REFACTORED/ant-software/components/Snippets/CtaBtn.tsx
+++ frontend/boilerplate/REFACTORED/ant-software/components/Snippets/CtaBtn.tsx
@@ -1,4 +1,11 @@
-const CtaBtn = () => {
-  return <div>CtaBtn</div>;
+import React from "react";
+
+const Button: React.FC = () => {
+  return (
+    <button className="btn btn-moving-gradient btn-moving-gradient--blue">
+      <a href="#contact">Enquire Now</a>
+    </button>
+  );
 };
-export default CtaBtn;
+
+export default Button;
