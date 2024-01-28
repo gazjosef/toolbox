da26fbaf53ccc3eee46b464b7533c60dd0fdffc9
Gareth Hind <garethjhind@gmail.com>
2024-01-28 22:33:14 +1100

    Feat: Added Navbar

--- frontend/boilerplate/REFACTORED/ant-software/components/Layout/Navbar.tsx
+++ frontend/boilerplate/REFACTORED/ant-software/components/Layout/Navbar.tsx
@@ -1,6 +1,79 @@
-import React from "react";
+import { useState } from "react";
+import { IconContext } from "react-icons";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+interface MenuItem {
+  title: string;
+  url: string;
+}
 
 const Navbar: React.FC = () => {
-  return <div>Navbar</div>;
+  const [toggle, setToggle] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setToggle((t) => !t);
+  };
+
+  const MenuItems: MenuItem[] = [
+    {
+      title: "Home",
+      url: "#home",
+    },
+    {
+      title: "About",
+      url: "#about",
+    },
+    {
+      title: "Services",
+      url: "#services",
+    },
+    {
+      title: "Contact",
+      url: "#contact",
+    },
+  ];
+
+  return (
+    <nav className="navbar | u-flex u-items-center | u-fs-nav">
+      {/* UNORDERED LIST */}
+      <ul
+        id="primary-navigation"
+        data-visible="false"
+        role="list"
+        className={`navbar__menu | u-flex u-items-center u-gap-2
+          ${toggle ? "navbar__active" : ""}
+            `}
+      >
+        {MenuItems.map((item: MenuItem, index: number) => {
+          return (
+            <li key={index} className="hover-underline-animation">
+              <a
+                className="u-clr-black-1000 u-ff-nav u-fw-semi-bold u-uppercase"
+                href={item.url}
+              >
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+
+      {/* TOGGLE BUTTON */}
+      <div
+        className="navbar__mobile-nav-toggle sr-only"
+        aria-label="Toggle navigation"
+        aria-expanded={toggle}
+      >
+        <IconContext.Provider value={{}}>
+          {toggle ? (
+            <FaTimes onClick={handleClick} />
+          ) : (
+            <FaBars onClick={handleClick} />
+          )}
+        </IconContext.Provider>
+      </div>
+    </nav>
+  );
 };
+
 export default Navbar;
