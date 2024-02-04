1dfe252fed67c718f7f45c8262774760ebd8e7af
Gareth Hind <garethjhind@gmail.com>
2024-02-04 22:28:55 +1100

    Fix: Favicon & Title

--- frontend/boilerplate/REFACTORED/weather-app2/components/Layout/Iphone.tsx
+++ frontend/boilerplate/REFACTORED/weather-app2/components/Layout/Iphone.tsx
@@ -7,10 +7,8 @@ const Iphone: React.FC<iPhoneProps> = () => {
   return (
     <div className="center">
       <div className="mobile | bg-white h-[580px] w-[276px] border-solid border-2 border-neutral-400 box-border rounded-[30px] shadow-[inset_0_0_8px_rgba(0,0,0,0.3)] | relative">
-        <div className="screen | bg-white h-[455px] w-[256px] border-solid border-[1px] border-neutral-400 mt-24 mx-auto | relative">
-          {/* WEATHER APP */}
-          {/* <WeatherApp /> */}
-        </div>
+        <h1>Hello World</h1>
+        {/* <div className="screen | bg-white h-[455px] w-[256px] border-solid border-[1px] border-neutral-400 mt-24 mx-auto | relative"></div>
         <div
           className="inner | bg-black h-[4px] w-[40px] box-border rounded-sm | absolute top-[32px] left-2/4 -translate-x-2/4 |
         before:content-[''] before:bg-black before:h-[8px] before:w-[8px] before:rounded-[50%] | before:absolute before:top-[-2px] before:left-[-22px] | 
@@ -26,7 +24,7 @@ const Iphone: React.FC<iPhoneProps> = () => {
         <ul className="sleep | absolute top-[90px] right-[-4px]">
           <li className="bg-neutral-50 h-[30px] w-[2px] mt-[10px] mx-0 rounded-tl-[4px] rounded-bl-[4px]"></li>
         </ul>
-        <div className="home | bg-white h-[40px] w-[40px] border-[2px] border-solid border-neutral-300 box-border rounded-[50%] shadow-[inset_0_0_7px_rgba(0,0,0,0.1)] | absolute bottom-[15px] left-2/4	-translate-x-2/4"></div>
+        <div className="home | bg-white h-[40px] w-[40px] border-[2px] border-solid border-neutral-300 box-border rounded-[50%] shadow-[inset_0_0_7px_rgba(0,0,0,0.1)] | absolute bottom-[15px] left-2/4	-translate-x-2/4"></div> */}
       </div>
     </div>
   );
--- frontend/boilerplate/REFACTORED/weather-app2/src/App.tsx
+++ frontend/boilerplate/REFACTORED/weather-app2/src/App.tsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import Iphone from "../components/Layout/Iphone.tsx";
 
 function App() {
   return (
     <>
+      <div className="min-h-screen | grid place-items-center">
+        <Iphone />
+      </div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
