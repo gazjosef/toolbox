ae7392aa9e34a2b7dd1cac59dff227945c0b9373
Gareth Hind <garethjhind@gmail.com>
2024-02-15 22:27:40 +1100

    Build: dontenv

--- frontend/boilerplate/REFACTORED/weather-app2/vite.config.ts
+++ frontend/boilerplate/REFACTORED/weather-app2/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+// Load environment variables from .env.local
+dotenv.config({ path: ".env.local" });
+
 export default defineConfig({
   plugins: [react()],
-})
+});
