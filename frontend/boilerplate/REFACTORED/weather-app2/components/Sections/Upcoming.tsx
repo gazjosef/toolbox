53797d290a6002d8b23e2571b4320b3d458fda35
Gareth Hind <garethjhind@gmail.com>
2024-02-22 22:10:25 +1100

    Refactor: Upcoming

--- frontend/boilerplate/REFACTORED/weather-app2/components/Sections/Upcoming.tsx
+++ frontend/boilerplate/REFACTORED/weather-app2/components/Sections/Upcoming.tsx
@@ -14,6 +14,20 @@ interface UpcomingProps {
 }
 
 const Upcoming: React.FC<UpcomingProps> = ({ upcomingData }) => {
+  // const timeConverter = (UNIX_timestamp: number): string => {
+  //   const a = new Date(UNIX_timestamp * 1000);
+
+  //   const hour: number = a.getHours();
+  //   const min: string = ("0" + a.getMinutes()).slice(-2);
+  //   const time: string = hour + ":" + min;
+
+  //   return time;
+  // };
+
+  if (upcomingData) {
+    console.log("upcomingData", upcomingData);
+  }
+
   return (
     <div className="h-[100px] w-full | flex flex-col justify-between">
       <h1 className="text-sm text-left">Hourly Forecast</h1>
