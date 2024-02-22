c390c63f4f286fb1960c378344ef45bbd1c1ddd8
Gareth Hind <garethjhind@gmail.com>
2024-02-22 22:59:33 +1100

    Refactor: Upcoming

--- frontend/boilerplate/REFACTORED/weather-app2/components/Sections/Upcoming.tsx
+++ frontend/boilerplate/REFACTORED/weather-app2/components/Sections/Upcoming.tsx
@@ -3,40 +3,69 @@ import { UpcomingData } from "./WeatherApp";
 // import { IconContext } from "react-icons";
 // import { iconConverter } from "../iconConverter";
 
-// interface HourlyForecast {
-//   dt: number;
-//   weather: { icon: string }[];
-//   main: { temp: number };
-// }
+interface HourData {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
 
 interface UpcomingProps {
   upcomingData: UpcomingData[] | null;
 }
 
 const Upcoming: React.FC<UpcomingProps> = ({ upcomingData }) => {
-  // const timeConverter = (UNIX_timestamp: number): string => {
-  //   const a = new Date(UNIX_timestamp * 1000);
+  const timeConverter = (UNIX_timestamp: number): string => {
+    const a = new Date(UNIX_timestamp * 1000);
 
-  //   const hour: number = a.getHours();
-  //   const min: string = ("0" + a.getMinutes()).slice(-2);
-  //   const time: string = hour + ":" + min;
+    const hour: number = a.getHours();
+    const min: string = ("0" + a.getMinutes()).slice(-2);
+    const time: string = hour + ":" + min;
 
-  //   return time;
-  // };
+    return time;
+  };
 
   // DISPLAY FUTURE DATA
-  const displayHour = upcomingData?.map((hour, index) => {
+  const displayHour = upcomingData.map((hour: HourData, index) => {
     console.log("hour", hour);
     return (
       <div
         key={index}
         className="hour | bg-sky-500 w-[35px] h-[70px] p-[5px] border-solid rounded-[10px] | flex flex-col items-center justify-between | text-[9px] text-white"
       >
-        {/* <div>{timeConverter(hour.dt)}</div>
+        <div>{timeConverter(hour.dt)}</div>
 
-          {hour.weather[0].icon}
+        {/* {hour.weather[0].icon} */}
 
-        <div>{JSON.stringify(hour.main.temp - 273.15).slice(0, 2)}&#8451;</div> */}
+        <div>{JSON.stringify(hour.main.temp - 273.15).slice(0, 2)}&#8451;</div>
       </div>
     );
   });
