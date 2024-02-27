b81d38b78cbdf510c15287c34439b9cae86d9110
Gareth Hind <garethjhind@gmail.com>
2024-02-27 21:34:23 +1100

    Refactor: Rm Unused Files

--- frontend/boilerplate/REFACTORED/booking-calendar/src/App.tsx
+++ frontend/boilerplate/REFACTORED/booking-calendar/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
-// import Header from "../components/Layout/Header.tsx";
+import Header from "../components/Layout/Header.tsx";
+import Calendar from "../components/Snippets/Calendar.tsx";
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Calendar />
     </>
   );
 }
