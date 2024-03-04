f4d95c8f6bc22c4c35667f407d5b627a0f177e15
Gareth Hind <garethjhind@gmail.com>
2024-03-04 22:30:59 +1100

    Build: TailwindCSS

--- frontend/boilerplate/REFACTORED/footy-tips/src/App.tsx
+++ frontend/boilerplate/REFACTORED/footy-tips/src/App.tsx
@@ -1,12 +1,17 @@
+// ** Import Share Component
 import Header from "../components/Layout/Header";
 import Footer from "../components/Layout/Footer";
+import Register from "../components/Sections/Register";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Header />
-      <h1>Hello World</h1>
+      {/* <Header /> */}
+      <main>
+        <h1>Hello World</h1>
+        <Register />
+      </main>
       <Footer />
     </>
   );
