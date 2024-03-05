24ef10ec46073a3b1f7531465deb5de5ee024a29
Gareth Hind <garethjhind@gmail.com>
2024-03-05 22:31:37 +1100

    Style: Added Poppins Font

--- frontend/boilerplate/REFACTORED/footy-tips/components/Layout/Header.tsx
+++ frontend/boilerplate/REFACTORED/footy-tips/components/Layout/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC = () => {
     <header className="bg-blue-400 w-screen py-4 | text-xl text-white">
       <div className="container mx-auto | flex items-center justify-between">
         <div className="space-x-2 | flex items-center justify-between | text-white">
-          <IconContext.Provider value={{ className: "u-fs-750" }}>
+          <IconContext.Provider value={{ className: "text-2xl" }}>
             <FaFootballBall />
           </IconContext.Provider>
           <h3 className="u-ml-1 u-uppercase">Footy Tips</h3>
