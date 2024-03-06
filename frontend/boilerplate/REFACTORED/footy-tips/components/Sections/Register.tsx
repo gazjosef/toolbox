7efa197cc9c584883ef6a30742fa40a66dc6c890
Gareth Hind <garethjhind@gmail.com>
2024-03-06 18:55:34 +1100

    Build: Register & Login

--- frontend/boilerplate/REFACTORED/footy-tips/components/Sections/Register.tsx
+++ frontend/boilerplate/REFACTORED/footy-tips/components/Sections/Register.tsx
@@ -1,14 +1,7 @@
-// import Link from "next/link";
-import React, {
-  // useContext,
-  useState,
-  ChangeEvent,
-  FormEvent,
-} from "react";
+import React, { useState, ChangeEvent, FormEvent } from "react";
 import { FaUser } from "react-icons/fa";
-// import AuthContext from "../../context/AuthContext";
 
-const Register = () => {
+const Register: React.FC = () => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -17,8 +10,6 @@ const Register = () => {
   });
 
   const { name, email, password, password2 } = formData;
-
-  //   const { dispatch } = useContext(AuthContext);
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFormData((prevState) => ({
@@ -33,88 +24,85 @@ const Register = () => {
     if (password !== password2) {
       console.log("Passwords do not match");
     } else {
-      //   const userData = {
-      //     name,
-      //     email,
-      //     password,
-      //   };
       // Dispatch userData or perform any other action with it
     }
   };
 
   return (
-    <form className="form" onSubmit={onSubmit}>
-      <div className="form__group">
-        <h2 className="secondary-heading | u-flex u-items-center">
-          <FaUser className="u-mr-1" /> Register
-        </h2>
-      </div>
-      {/* ENTER YOUR NAME */}
-      <div className="form__group">
-        <input
-          type="text"
-          id="name"
-          className="form__control"
-          value={name}
-          placeholder="Enter your name"
-          onChange={onChange}
-          autoComplete="on"
-          name="name"
-        />
-      </div>
-      {/* ENTER YOUR EMAIL */}
-      <div className="form__group">
-        <input
-          type="email"
-          id="email"
-          className="form__control"
-          value={email}
-          placeholder="Enter your email"
-          onChange={onChange}
-          autoComplete="on"
-          name="email"
-        />
-      </div>
-      {/* ENTER YOUR PASSWORD */}
-      <div className="form__group">
-        <input
-          type="password"
-          id="password"
-          className="form__control"
-          value={password}
-          placeholder="Enter password"
-          onChange={onChange}
-          autoComplete="on"
-          name="password"
-        />
-      </div>
-      {/* ENTER YOUR PASSWORD2 */}
-      <div className="form__group">
-        <input
-          type="password"
-          id="password2"
-          className="form__control"
-          value={password2}
-          placeholder="Confirm password"
-          onChange={onChange}
-          autoComplete="on"
-          name="password2"
-        />
-      </div>
+    <section>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form__group">
+          <h2 className="secondary-heading | u-flex u-items-center">
+            <FaUser className="u-mr-1" /> Register
+          </h2>
+        </div>
+        {/* ENTER YOUR NAME */}
+        <div className="form__group">
+          <input
+            type="text"
+            id="name"
+            className="form__control"
+            value={name}
+            placeholder="Enter your name"
+            onChange={onChange}
+            autoComplete="on"
+            name="name"
+          />
+        </div>
+        {/* ENTER YOUR EMAIL */}
+        <div className="form__group">
+          <input
+            type="email"
+            id="email"
+            className="form__control"
+            value={email}
+            placeholder="Enter your email"
+            onChange={onChange}
+            autoComplete="on"
+            name="email"
+          />
+        </div>
+        {/* ENTER YOUR PASSWORD */}
+        <div className="form__group">
+          <input
+            type="password"
+            id="password"
+            className="form__control"
+            value={password}
+            placeholder="Enter password"
+            onChange={onChange}
+            autoComplete="on"
+            name="password"
+          />
+        </div>
+        {/* ENTER YOUR PASSWORD2 */}
+        <div className="form__group">
+          <input
+            type="password"
+            id="password2"
+            className="form__control"
+            value={password2}
+            placeholder="Confirm password"
+            onChange={onChange}
+            autoComplete="on"
+            name="password2"
+          />
+        </div>
 
-      <div className="form__group">
-        <p>Already a member? Login here.</p>
-      </div>
+        <div className="form__group">
+          <p>Already a member? Login here.</p>
+        </div>
 
-      <div className="form__group">
-        <button
-          type="submit"
-          className="btn btn-block | u-bg-primary-400 u-clr-white-0"
-        >
-          Submit
-        </button>
-      </div>
-    </form>
+        <div className="form__group">
+          <button
+            type="submit"
+            className="btn btn-block | u-bg-primary-400 u-clr-white-0"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
   );
 };
 
--- frontend/boilerplate/REFACTORED/footy-tips/src/App.tsx
+++ frontend/boilerplate/REFACTORED/footy-tips/src/App.tsx
@@ -1,6 +1,7 @@
 // ** Import Share Component
 import Layout from "../components/Layout/Layout";
 import Register from "../components/Sections/Register";
+import Form from "../components/Snippets/Form";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Layout>
         <main className="container | min-h-[85vh] mx-auto p-12">
           <div className="mx-auto">
+            <Form />
             <Register />
           </div>
         </main>
