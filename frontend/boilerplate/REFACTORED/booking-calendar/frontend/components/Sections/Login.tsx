3266a504259096272df50ddfc8774bfc079f3bb8
Gareth Hind <garethjhind@gmail.com>
2024-03-12 22:02:50 +1100

    Build: Backend Footy Tips

--- frontend/boilerplate/REFACTORED/booking-calendar/frontend/components/Sections/Login.tsx
+++ frontend/boilerplate/REFACTORED/booking-calendar/frontend/components/Sections/Login.tsx
@@ -1,11 +1,14 @@
+import { IconContext } from "react-icons";
 import { FaUser } from "react-icons/fa";
 
 const Login: React.FC = () => {
   return (
     <section className="min-h-[85vh] | grid place-items-center">
       <form className="max-w-md w-1/3 mx-auto border-2 p-4 rounded-lg space-y-6">
-        <div className="space-x-2 mx-auto | flex items-center justify-center | text-2xl">
-          <FaUser />
+        <div className="space-x-2 mx-auto | flex items-center justify-center | text-2xl uppercase">
+          <IconContext.Provider value={{ size: "2rem" }}>
+            <FaUser />
+          </IconContext.Provider>
           <h2>Login</h2>
         </div>
         <div>
