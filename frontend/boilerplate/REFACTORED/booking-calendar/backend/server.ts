6da2cb8f5ac5bfebf8c54b4ae66dd395c2ad944c
Gareth Hind <garethjhind@gmail.com>
2024-03-17 21:35:06 +1100

    Express Async Handler

--- frontend/boilerplate/REFACTORED/booking-calendar/backend/server.ts
+++ frontend/boilerplate/REFACTORED/booking-calendar/backend/server.ts
@@ -2,13 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import goalRoutes from "./routes/goalRoutes";
+import { errorHandler } from "./middleware/errorMiddleware";
 
 const startServer = async () => {
   const port: number | string = process.env.PORT || 5001;
 
   const app = express();
 
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
   app.use("/api/goals", goalRoutes);
+  app.use(errorHandler);
 
   app.listen(port, () => console.log(`Server started on port ${port}`));
 };
