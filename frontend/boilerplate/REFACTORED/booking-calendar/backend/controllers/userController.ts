551abe79895eef004956ed2eab5b0324c09d20c6
Gareth Hind <garethjhind@gmail.com>
2024-03-19 21:50:33 +1100

    Build: Register User

--- frontend/boilerplate/REFACTORED/booking-calendar/backend/controllers/userController.ts
+++ frontend/boilerplate/REFACTORED/booking-calendar/backend/controllers/userController.ts
@@ -1,20 +1,41 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import asyncHandler from "express-async-handler";
+import { User } from "../models/userModel";
 
 // @desc    Register User
 // @route   POST /api/users
 // @access  Public
-export const registerUser = (req: Request, res: Response) => {
-  res.json({ message: "Register User" });
-};
+export const registerUser = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      res.status(400);
+      throw new Error("Please add all fields");
+    }
+
+    // Check if user exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400);
+      throw new Error("User already exists");
+    }
+
+    res.json({ message: "Register User" });
+  }
+);
 // @desc    Authenticate A User
 // @route   POST /api/users/login
 // @access  Public
-export const loginUser = (req: Request, res: Response) => {
+export const loginUser = asyncHandler(async (req: Request, res: Response) => {
   res.json({ message: "Login User" });
-};
+});
 // @desc    Get User Data
 // @route   GET /api/users/me
 // @access  Public
-export const getMe = (req: Request, res: Response) => {
+export const getMe = asyncHandler(async (req: Request, res: Response) => {
   res.json({ message: "User Data Display" });
-};
+});
