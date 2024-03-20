c02efb16b319b3b66e0da5ca12fa7dfdff507d18
Gareth Hind <garethjhind@gmail.com>
2024-03-20 21:34:30 +1100

    Build: Protect Goal Routes

--- frontend/boilerplate/REFACTORED/booking-calendar/backend/routes/goalRoutes.ts
+++ frontend/boilerplate/REFACTORED/booking-calendar/backend/routes/goalRoutes.ts
@@ -5,10 +5,11 @@ import {
   updateGoal,
   deleteGoal,
 } from "../controllers/goalController";
+import { protect } from "../middleware/authMiddleware";
 
 const router: Router = Router();
 
-router.route("/").get(getGoals).post(setGoal);
-router.route("/:id").put(updateGoal).delete(deleteGoal);
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 
 export default router;
