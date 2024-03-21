f615e28185d3fcb72bd7d5b2fb24cacfa094feb3
Gareth Hind <garethjhind@gmail.com>
2024-03-21 22:23:01 +1100

    Refactor: Header

--- frontend/boilerplate/REFACTORED/portfolio/.next/static/chunks/webpack.js
+++ frontend/boilerplate/REFACTORED/portfolio/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "39927abb1cdd472e"; }
+/******/ 		__webpack_require__.h = function() { return "e80076d29e381e00"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/boilerplate/REFACTORED/portfolio/components/Layout/Header.tsx
+++ frontend/boilerplate/REFACTORED/portfolio/components/Layout/Header.tsx
@@ -5,12 +5,10 @@ import Navbar from "./Navbar";
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <div className="container | u-flex u-justify-between u-uppercase">
+      <div className="container">
         <Link href="/">
-          {/* <a className="u-fw-semi-bold u-text-black u-uppercase"> */}
           <span className="u-fw-bold u-text-primary-300">Gareth Hind</span>{" "}
           <span className="header__span">Web Developer</span>
-          {/* </a> */}
         </Link>
         <Navbar />
       </div>
